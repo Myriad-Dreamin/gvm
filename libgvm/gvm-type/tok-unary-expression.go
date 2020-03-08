f47aa13498be72a4b345bc42e4124ca67f54d016
Myriad-Dreamin <camiyoru@gmail.com>
2020-03-08 16:02:25 +0800

    add todo functions

--- libgvm/gvm-type/tok-unary-expression.go
+++ libgvm/gvm-type/tok-unary-expression.go
@@ -1,9 +1,6 @@
 package gvm_type
 
-import (
-	"fmt"
-	"github.com/Myriad-Dreamin/gvm/internal/abstraction"
-)
+import "github.com/Myriad-Dreamin/gvm/internal/abstraction"
 
 type UnaryExpression struct {
 	Type abstraction.RefType `json:"type"`
@@ -12,22 +9,13 @@ type UnaryExpression struct {
 }
 
 func (u UnaryExpression) GetGVMTok() abstraction.TokType {
-	return TokUnaryExpression
+	panic("implement me")
 }
 
 func (u UnaryExpression) GetGVMType() abstraction.RefType {
-	return u.Type
+	panic("implement me")
 }
 
 func (u UnaryExpression) Eval(g *abstraction.ExecCtx) (abstraction.Ref, error) {
-	l, err := u.Left.Eval(g)
-	if err != nil {
-		return nil, err
-	}
-	switch u.Sign {
-	case SignLNot:
-		return LNot(l)
-	default:
-		return nil, fmt.Errorf("unknown sign_type: %v", u.Sign)
-	}
+	panic("implement me")
 }
