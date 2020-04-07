bbb7c696022e136b252ced3a8a5b4b5626339ee2
Myriad-Dreamin <35292584+Myriad-Dreamin@users.noreply.github.com>
2020-04-07 15:30:07 +0800

    fix bug: gvm.Continue does not save frames (#16)
    * fix continue interface

--- libgvm/run.go
+++ libgvm/run.go
@@ -4,50 +4,18 @@ import (
 	"github.com/Myriad-Dreamin/gvm/internal/abstraction"
 )
 
-func Step(g abstraction.Machine) error {
+func ProcessRunException(g *abstraction.ExecCtx, err error) error {
 
-	c := abstraction.ExecCtx{Machine: g}
-	if err := loadFrameFromDisk(&c); err != nil {
-		return err
-	}
-
-	return Iter(&c)
-}
-
-func Continue(g abstraction.Machine) error {
-	c := abstraction.ExecCtx{Machine: g}
-	if err := loadFrameFromDisk(&c); err != nil {
-		return err
-	}
-
-	return _Continue(&c)
-}
-
-func _Continue(g *abstraction.ExecCtx) (err error) {
-	for ; err == nil; err = Iter(g) {
-	}
-
-	return err
-}
-
-//trapCallFunc
-func Run(g abstraction.Machine, fn string, reportSaveError func(err error)) (err error) {
-
-	var c = &abstraction.ExecCtx{Machine: g, Depth: 0, This: make(abstraction.Locals)}
-	err = PushFrame(c, fn)
-	for err == nil {
-		err = _Continue(c)
-
-		if err == OutOfRange {
-			err = popFrame(c)
-		} else if trap, ok := err.(abstraction.Trap); ok {
-			err = trap.DoTrap(c)
-		}
-	}
-	if err != StopUnderFlow {
-		reportSaveError(saveFrame(c))
+	if err == OutOfRange {
+		err = popFrame(g)
+	} else if trap, ok := err.(abstraction.Trap); ok {
+		err = trap.DoTrap(g)
 	}
 	return err
+}
+
+func SaveFrame(g *abstraction.ExecCtx) error {
+	return saveFrame(g)
 }
 
 func Iter(g *abstraction.ExecCtx) (err error) {
@@ -59,6 +27,46 @@ func Iter(g *abstraction.ExecCtx) (err error) {
 		return err
 	}
 	return inst.Exec(g)
+}
+
+func _Continue(g *abstraction.ExecCtx, err error, reportSaveError func(err error)) error {
+	for err == nil {
+		for ; err == nil; err = Iter(g) {
+		}
+		err = ProcessRunException(g, err)
+	}
+	if err != StopUnderFlow {
+		reportSaveError(saveFrame(g))
+	}
+
+	return err
+}
+
+func Step(g abstraction.Machine) error {
+
+	c := abstraction.ExecCtx{Machine: g}
+	if err := loadFrameFromDisk(&c); err != nil {
+		return err
+	}
+
+	return Iter(&c)
+}
+
+func Continue(g abstraction.Machine, reportSaveError func(err error)) error {
+	c := abstraction.ExecCtx{Machine: g}
+	if err := loadFrameFromDisk(&c); err != nil {
+		return err
+	}
+
+	return _Continue(&c, nil, reportSaveError)
+}
+
+//trapCallFunc
+func Run(g abstraction.Machine, fn string, reportSaveError func(err error)) (err error) {
+
+	var c = abstraction.ExecCtx{Machine: g, Depth: 0, This: make(abstraction.Locals)}
+	err = PushFrame(&c, fn)
+	return _Continue(&c, err, reportSaveError)
 }
 
 func validate(g *abstraction.ExecCtx) error {
